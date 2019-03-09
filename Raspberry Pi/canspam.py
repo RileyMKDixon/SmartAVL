60d09daa165d364fad5d93f9a823463b7a53b100
crashb <snickerless1@shaw.ca>
2019-03-09 12:06:43 -0700

    added canspam.py which uses python-can config

---
+++ Raspberry Pi/canspam.py
@@ -0,0 +1,20 @@
+# canspam.py:
+# spams messages to default CAN bus
+# (specified in python-can configuration file)
+
+import can
+import time
+
+def send_msg(id):
+    bus = can.interface.Bus() # default bus
+    msg = can.Message(arbitration_id=id, data=[1, 2, 3, 4, 5, 6, 7, 8], is_extended_id=False)
+    bus.send(msg)
+    
+def spam_msgs():
+    while True:
+        for i in range(0x7ff):
+            send_msg(i)
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    spam_msgs()
