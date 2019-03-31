b6288d3b1619517293ed3c34d2a51e598a5cab65
crash <snickerless1@shaw.ca>
2019-03-30 18:04:48 -0600

    changed canlog.py to use kernel filtering

--- Raspberry Pi/canlog.py
+++ Raspberry Pi/canlog.py
@@ -7,16 +7,35 @@ from canencoder import CANEncoder
 
 log_file = "can.log"
 
+bus = can.interface.Bus()
+
+
+def setup_bus_filters():
+    obd2_filters = [
+        {
+            "can_id": 0x7df,
+            "can_mask": 0x7ff
+        },
+        {
+            "can_id": 0x7e8,
+            "can_mask": 0x7ff
+        },
+    ]
+    bus.set_filters(obd2_filters)
+
+
 def log_msg_local(msg):
     with open(log_file, "a+") as f:
         j = json.dumps(msg, cls=CANEncoder)
         f.write(j + "\n")
 
+
 def log_msgs():
-    bus = can.interface.Bus()
     while True:
         msg = bus.recv()
         log_msg_local(msg)
 
+
 if __name__ == "__main__":
+    setup_bus_filters()
     log_msgs()
