ad37a08eff266c47ec059a8ba8ab223cd7b33c26
crash <snickerless1@shaw.ca>
2019-04-02 23:53:17 -0600

    added coolant temperature to obd2_stats.py

--- Raspberry Pi/obd2_stats.py
+++ Raspberry Pi/obd2_stats.py
@@ -4,46 +4,45 @@
 from obd2_utils import *
 from obd2_supported import find_all_supported
 
-PID_SPEED = 0x0D
-PID_RPM = 0x0C
-PID_FUEL_TANK_LEVEL = 0x2F
-PID_FUEL_PRESSURE = 0x0A
-PID_OIL_TEMP = 0x5C
-PID_DISTANCE_SINCE_CLEAR = 0x31
 PID_MONITOR_STATUS = 0x01
+PID_COOLANT_TEMP = 0x05
+PID_FUEL_PRESSURE = 0x0A
+PID_RPM = 0x0C
+PID_SPEED = 0x0D
+PID_FUEL_TANK_LEVEL = 0x2F
+PID_DISTANCE_SINCE_CLEAR = 0x31
+PID_OIL_TEMP = 0x5C
 
 
 # Extracts relevant data from a CAN bus message given the OBD2 PID.
 def extract_data(msg, pid):
-    if pid == PID_SPEED:
-        return extract_speed(msg)
-    if pid == PID_RPM:
-        return extract_rpm(msg)
-    if pid == PID_FUEL_TANK_LEVEL:
-        return extract_fuel_tank_level(msg)
-    if pid == PID_FUEL_PRESSURE:
-        return extract_fuel_pressure(msg)
-    if pid == PID_OIL_TEMP:
-        return extract_oil_temp(msg)
-    if pid == PID_DISTANCE_SINCE_CLEAR:
-        return extract_distance_since_clear(msg)
     if pid == PID_MONITOR_STATUS:
         return extract_monitor_status(msg)
+    if pid == PID_COOLANT_TEMP:
+        return extract_coolant_temp(msg)
+    if pid == PID_FUEL_PRESSURE:
+        return extract_fuel_pressure(msg)
+    if pid == PID_RPM:
+        return extract_rpm(msg)
+    if pid == PID_SPEED:
+        return extract_speed(msg)
+    if pid == PID_FUEL_TANK_LEVEL:
+        return extract_fuel_tank_level(msg)
+    if pid == PID_DISTANCE_SINCE_CLEAR:
+        return extract_distance_since_clear(msg)
+    if pid == PID_OIL_TEMP:
+        return extract_oil_temp(msg)
 
 
-# Extracts vehicle speed from a CAN message (PID 0x0D).
-def extract_speed(msg):
-    return int(msg.data[3])
+# Extracts check engine light status from a CAN message (PID 0x01).
+# Returns 1 if check engine light is on, and 0 otherwise.
+def extract_monitor_status(msg):
+    return int(msg.data[3]) >> 7
 
 
-# Extracts RPM from a CAN message (PID 0x0C).
-def extract_rpm(msg):
-    return int.from_bytes(msg.data[3:5], byteorder='big') / 4
-
-
-# Extracts fuel tank level from a CAN message (PID 0x2F).
-def extract_fuel_tank_level(msg):
-    return 100 / 255 * int(msg.data[3])
+# Extracts coolant temperature from a CAN message (PID 0x05).
+def extract_coolant_temp(msg):
+    return int(msg.data[3]) - 40
 
 
 # Extracts fuel pressure from a CAN message (PID 0x0A).
@@ -51,9 +50,19 @@ def extract_fuel_pressure(msg):
     return 3 * int(msg.data[3])
 
 
-# Extracts engine oil temperature from a CAN message (PID 0x5C).
-def extract_oil_temp(msg):
-    return int(msg.data[3]) - 40
+# Extracts RPM from a CAN message (PID 0x0C).
+def extract_rpm(msg):
+    return int.from_bytes(msg.data[3:5], byteorder='big') / 4
+
+
+# Extracts vehicle speed from a CAN message (PID 0x0D).
+def extract_speed(msg):
+    return int(msg.data[3])
+
+
+# Extracts fuel tank level from a CAN message (PID 0x2F).
+def extract_fuel_tank_level(msg):
+    return 100 / 255 * int(msg.data[3])
 
 
 # Extracts distance since codes have been cleared from a CAN message (PID 0x31).
@@ -61,10 +70,9 @@ def extract_distance_since_clear(msg):
     return int.from_bytes(msg.data[3:5], byteorder='big')
 
 
-# Extracts check engine light status from a CAN message (PID 0x01).
-# Returns 1 if check engine light is on, and 0 otherwise.
-def extract_monitor_status(msg):
-    return int(msg.data[3]) >> 7
+# Extracts engine oil temperature from a CAN message (PID 0x5C).
+def extract_oil_temp(msg):
+    return int(msg.data[3]) - 40
 
 
 # Gets OBD2 statistics for a supplied list of PIDs.
